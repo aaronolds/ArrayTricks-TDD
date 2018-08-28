2378f30ba9af5a8d17e958324e82161c066c8119
Aaron P. Olds <aaron.olds@gmail.com>
2018-08-28 17:41:07 -0400

    updated

--- ArrayTricks/Set.cs
+++ ArrayTricks/Set.cs
@@ -2,24 +2,51 @@ using System;
 
 public class Set
 {
-    private int _size = 0;
-    private string[] _set = new string[10];
-    
+    private string[] _set;
+
+    public Set(int size = 10)
+    {
+        _set = new string[size];
+    }
+
+    public int Size { get; set; } = 0;
+
     public bool isEmpty()
     {
-        return _size == 0;
+        return Size == 0;
     }
-    
+
     public void add(string elem)
     {
-        _set[_size++] = elem;
+        if (indexOf(elem) >= 0) return;
+
+        if (_set.Length == Size)
+        {
+            Array.Resize<string>(ref _set, Size + 1);
+        }
+
+        _set[Size++] = elem;
+    }
+
+    public int indexOf(string elem)
+    {
+        for (int i = 0; i < Size; i++)
+        {
+            if (_set[i].Equals(elem)) return i;
+        }
+        return -1;
+    }
+
+    public bool contains(string elem)
+    {
+        return indexOf(elem) >= 0;
     }
 
     public void remove(string elem)
     {
-        if (_size == 0) return;
+        if (Size == 0) return;
         int i = 0;
-        for (int j = 0; j < _size; j++)
+        for (int j = 0; j < Size; j++)
         {
             if (_set[j] != elem)
             {
--- ArrayTricksTests/UnitTest1.cs
+++ ArrayTricksTests/UnitTest1.cs
@@ -3,13 +3,58 @@ using Xunit;
 
 namespace ArrayTricks
 {
-    public class UnitTest1
+    public class SetUnitTest
     {
-        [Fact]
-        public void Test1()
+        private Set _empty;
+        private Set _one;
+        private Set _two;
+        public SetUnitTest()
         {
-            var empty = new Set();
-            Assert.True(empty.isEmpty());
+            _empty = new Set();
+            _one = new Set(1);
+            _two = new Set(2);
+            _one.add("a");
+
+            _two.add("a");
+            _two.add("b");
+        }
+
+        [Fact]
+        public void IsEmptyTest()
+        {
+            Assert.True(_empty.isEmpty());
+            Assert.False(_one.isEmpty());
+        }
+
+        [Fact]
+        public void AddItemTest()
+        {
+            Assert.False(_one.isEmpty());
+            Assert.False(_two.isEmpty());
+            Assert.True(_one.Size == 1);
+            Assert.True(_two.Size == 2);
+        }
+
+        [Fact]
+        public void DoesTheSetContain()
+        {
+            Assert.False(_empty.contains("a"));
+            Assert.True(_one.contains("a"));
+            Assert.True(_two.contains("b"));
+        }
+
+        [Fact]
+        public void AddDuplicate()
+        {
+            _two.add("b");
+            Assert.True(_two.Size == 2);
+        }
+
+        [Fact]
+        public void AddElementStackOverflow()
+        {
+            _two.add("c");
+            Assert.True(_two.Size == 3);
         }
     }
 }
